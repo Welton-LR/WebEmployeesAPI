0b3978a961ffb089a8456d9cc6b59742d3988d0c
Welton <weltonrdesigner@gmail.com>
2024-03-27 21:16:05 -0300

    DepartamentoEnum

---
+++ WebEmployeesAPI/Enums/DepartamentoEnum.cs
@@ -0,0 +1,11 @@
+﻿namespace WebEmployeesAPI.Enums
+{
+    public enum DepartamentoEnum
+    {
+        RH,
+        Financeiro,
+        Compras,
+        Atendimento,
+        Zeladoria
+    }
+}
--- WebEmployeesAPI/Models/FuncionarioModel.cs
+++ WebEmployeesAPI/Models/FuncionarioModel.cs
@@ -1,11 +1,13 @@
-﻿namespace WebEmployeesAPI.Models
+﻿using WebEmployeesAPI.Enums;
+
+namespace WebEmployeesAPI.Models
 {
     public class FuncionarioModel
     {
         public int Id { get; set; }
         public string Nome { get; set; }
         public string Sobrenome { get; set; }
-        public string Departamento { get; set; }
+        public DepartamentoEnum Departamento { get; set; }
         public bool Ativo { get; set; }
         public string Turno { get; set; }
         public DateTime DataDeCriacao { get; set; } = DateTime.Now.ToLocalTime();
