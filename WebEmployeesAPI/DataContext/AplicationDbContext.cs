b2dd67e7f77a0b379268c5c48b53f8b8697fa8c4
Welton <weltonrdesigner@gmail.com>
2024-03-27 22:07:59 -0300

    ConnectionString

---
+++ WebEmployeesAPI/DataContext/AplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebEmployeesAPI.Models;
+
+namespace WebEmployeesAPI.DataContext
+{
+    public class AplicationDbContext : DbContext
+    {
+        public AplicationDbContext(DbContextOptions<AplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<FuncionarioModel> Funcionario { get; set;}
+
+    }
+}
