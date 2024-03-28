40519eb9f5f65828af4ffffacbccf2386cbf0337
Welton <weltonrdesigner@gmail.com>
2024-03-27 21:06:57 -0300

    FuncionarioModel

--- WebEmployeesAPI/Models/ServiceResponse.cs
+++ WebEmployeesAPI/Models/ServiceResponse.cs
@@ -2,13 +2,6 @@
 {
     public class ServiceResponse
     {
-        public int Id { get; set; }
-        public string Nome { get; set; }
-        public string Sobrenome { get; set; }
-        public string Departamento { get; set; }
-        public bool Ativo { get; set; }
-        public string Turno { get; set; }
-        public DateTime DataDeCriacao { get; set; } = DateTime.Now.ToLocalTime();
-        public DateTime DataDeModificacao { get; set; } = DateTime.Now.ToLocalTime();
+        
     }
 }
