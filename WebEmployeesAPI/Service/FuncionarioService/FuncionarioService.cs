983bc5205f5538a0d74434d6c7238b79cd23c6bf
Welton <weltonrdesigner@gmail.com>
2024-04-03 22:12:37 -0300

    Implementacao da Interface

--- WebEmployeesAPI/Service/FuncionarioService/FuncionarioService.cs
+++ WebEmployeesAPI/Service/FuncionarioService/FuncionarioService.cs
@@ -4,7 +4,32 @@ namespace WebEmployeesAPI.Service.FuncionarioService
 {
     public class FuncionarioService : InterfaceFuncionario
     {
+        public Task<ServiceResponse<List<FuncionarioModel>>> CreateFuncionarios(FuncionarioModel novoFuncionario)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ServiceResponse<List<FuncionarioModel>>> DeleteFuncionario(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ServiceResponse<FuncionarioModel>> GetFuncionarioById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
         public Task<ServiceResponse<List<FuncionarioModel>>> GetFuncionarios()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ServiceResponse<List<FuncionarioModel>>> InativaFuncionario(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ServiceResponse<List<FuncionarioModel>>> UpdateFuncionario(FuncionarioModel editadoFuncionario)
         {
             throw new NotImplementedException();
         }
