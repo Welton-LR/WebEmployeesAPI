244ecef654b2ae226aabf664fd367583b8c2a787
Welton <weltonrdesigner@gmail.com>
2024-04-07 19:04:11 -0300

    GetFuncionarios

--- WebEmployeesAPI/Service/FuncionarioService/FuncionarioService.cs
+++ WebEmployeesAPI/Service/FuncionarioService/FuncionarioService.cs
@@ -1,9 +1,15 @@
-﻿using WebEmployeesAPI.Models;
+﻿using System.Collections.Generic;
+using WebEmployeesAPI.DataContext;
+using WebEmployeesAPI.Models;
 
 namespace WebEmployeesAPI.Service.FuncionarioService
 {
     public class FuncionarioService : InterfaceFuncionario
-    {
+    {   private readonly AplicationDbContext _context;
+        public FuncionarioService(AplicationDbContext context)
+        {
+            _context = context;
+        }
         public Task<ServiceResponse<List<FuncionarioModel>>> CreateFuncionarios(FuncionarioModel novoFuncionario)
         {
             throw new NotImplementedException();
@@ -19,9 +25,21 @@ namespace WebEmployeesAPI.Service.FuncionarioService
             throw new NotImplementedException();
         }
 
-        public Task<ServiceResponse<List<FuncionarioModel>>> GetFuncionarios()
+        public async Task<ServiceResponse<List<FuncionarioModel>>> GetFuncionarios()
         {
-            throw new NotImplementedException();
+            ServiceResponse<List<FuncionarioModel>> serviceResponse = new ServiceResponse<List<FuncionarioModel>>();
+
+            try
+            {
+                serviceResponse.Dados = _context.Funcionario.ToList();
+            }
+            catch (Exception ex)
+            {
+                serviceResponse.Mensagem = ex.Message;
+                serviceResponse.Sucesso = false;
+            }
+            return serviceResponse;
+
         }
 
         public Task<ServiceResponse<List<FuncionarioModel>>> InativaFuncionario(int id)
